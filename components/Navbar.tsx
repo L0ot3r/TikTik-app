cf9375d9a42a82cc6e4802656d30c6c98f83ce5e
leoville <sebastien.leoville@gmail.com>
2022-08-08 20:22:15 +0200

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
+
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
+
+import { createOrGetUser } from '../utils';
+import Logo from '../utils/tiktik-logo.png';
+
+import useAuthStore from '../store/authStore';
+
+const Navbar = () => {
+	const { userProfile, addUser, removeUser } = useAuthStore();
+
+	return (
+		<div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+			<Link href='/'>
+				<div className='w-[100px] md:w-[130px]'>
+					<Image
+						className='cursor-pointer'
+						src={Logo}
+						alt='Tiktik logo'
+						layout='responsive'
+						/>
+				</div>
+			</Link>
+
+			<div>SEARCH</div>
+
+			<div>
+				{userProfile ? (
+					<div className='flex gap-5 md:gap-10'>
+						<Link href='/upload'>
+							<button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
+								<IoMdAdd className='text-xl' />{' '}
+								<span className='uppercase hidden md:block'>upload</span>
+							</button>
+						</Link>
+						{userProfile.image && (
+							<Link href='/'>
+								<>
+									<Image
+										src={userProfile.image}
+										alt={'profile photo'}
+										width={40}
+										height={40}
+										className='rounded-full cursor-pointer'
+										/>
+								</>
+							</Link>
+						)}
+						<button
+							type='button'
+							className='px-2 hover:bg-gray-200 rounded-full'
+							onClick={() => {
+								googleLogout();
+								removeUser();
+							}}
+							>
+							<AiOutlineLogout color='crimson' fontSize={21} />
+						</button>
+					</div>
+				) : (
+					<GoogleLogin
+					onSuccess={(response) => createOrGetUser(response, addUser)}
+					onError={() => console.log('Error: ')}
+					/>
+					)}
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
---
+++ utils/index.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const createOrGetUser = async (response: any, addUser: any) => {
+	const decoded: {
+		name: string;
+		picture: string;
+		sub: string;
+		given_name: string;
+		family_name: string;
+	} = jwt_decode(response.credential);
+
+	const { name, sub, picture, family_name, given_name } = decoded;
+
+	const user = {
+		_id: sub,
+		_type: 'user',
+		userName: name,
+		image: picture,
+	};
+
+	addUser(user);
+
+	await axios.post(`http://localhost:3000/api/auth`, user);
+};
